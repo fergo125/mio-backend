617980cc2910644a12070a1064e8be534f06c775
Alonso Vega <lavb91@gmail.com>
2016-08-20 22:21:01 -0600

    Removing unnecesary fields on serializers.

--- miocimar/api/serializers.py
+++ miocimar/api/serializers.py
@@ -6,18 +6,15 @@ from django.views.decorators.csrf import csrf_exempt
 class TideRegionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TideRegion
-        fields = ('url','id','name','icon_url')
 
 class TideEntrySerializer(serializers.HyperlinkedModelSerializer):
     #id = serializers.HyperlinkedIdentityField(many=True, view_name='tides-week')
     class Meta:
         model = TideEntry
-        fields = ('id','tide_region','date','tide_height','is_high_tide')
 
 class LocalForecastSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = LocalForecast
-        fields=('url', 'id', 'name', 'icon_url', 'map_url')
 
 class LocalForecastEntryCreateSerializer(serializers.ModelSerializer):
     local_forecast = serializers.PrimaryKeyRelatedField(queryset=LocalForecast.objects.all())
@@ -28,9 +25,7 @@ class LocalForecastEntryCreateSerializer(serializers.ModelSerializer):
 class LocalForecastEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = LocalForecastEntry
-        fields=('pk', 'date', 'wave_direction', 'wave_height_sig', 'wave_height_max', 'wave_period', 'wind_direction', 'wind_speed', 'wind_burst')
 
 class WaveWarningSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = WaveWarning
-        fields = ('url','id','title','level','date','text')
